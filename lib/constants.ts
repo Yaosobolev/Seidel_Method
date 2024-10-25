892106aaea6d09791514818a80c2676a941f2869
Yaosobolev <yaosobolev@gmail.com>
2024-10-25 09:10:28 +0500

    add data

--- lib/constants.ts
+++ lib/constants.ts
@@ -74,34 +74,62 @@ export const accuracy = [
 // ];
 
 // export const b = [-0.654 / 8, 24.016 / 4.056, -54.567 / 0.2, 64.78 / 12];
+// export const A: AMatrixItem[][] = [
+//   [
+//     { name: "x1", value: 12 },
+//     { name: "x2", value: -3.8 },
+//     { name: "x3", value: 1 },
+//     { name: "x4", value: 3 },
+//   ],
+//   [
+//     { name: "x1", value: 8 },
+//     { name: "x2", value: 11 },
+//     { name: "x3", value: -1 },
+//     { name: "x4", value: -0.07 },
+//   ],
+//   [
+//     { name: "x1", value: 4.056 },
+//     { name: "x2", value: 0.3 },
+//     { name: "x3", value: -5.3 },
+//     { name: "x4", value: 0.11 },
+//   ],
+//   [
+//     { name: "x1", value: 0.2 },
+//     { name: "x2", value: 5.77 },
+//     { name: "x3", value: 2.8 },
+//     { name: "x4", value: -15 },
+//   ],
+// ];
+
+// export const b = [-64.78, 0.654, -24.016, 54.567];
 export const A: AMatrixItem[][] = [
   [
-    { name: "x1", value: 12 },
-    { name: "x2", value: -3.8 },
+    { name: "x1", value: 10 },
+    { name: "x2", value: 2 },
     { name: "x3", value: 1 },
-    { name: "x4", value: 3 },
+    { name: "x4", value: 4 },
   ],
   [
-    { name: "x1", value: 8 },
-    { name: "x2", value: 11 },
-    { name: "x3", value: -1 },
-    { name: "x4", value: -0.07 },
+    { name: "x1", value: 2 },
+    { name: "x2", value: 110 },
+    { name: "x3", value: -2 },
+    { name: "x4", value: 7 },
   ],
   [
-    { name: "x1", value: 4.056 },
-    { name: "x2", value: 0.3 },
-    { name: "x3", value: -5.3 },
-    { name: "x4", value: 0.11 },
+    { name: "x1", value: 6.666 },
+    { name: "x2", value: 2.3 },
+    { name: "x3", value: -15.6 },
+    { name: "x4", value: 1 },
   ],
   [
-    { name: "x1", value: 0.2 },
-    { name: "x2", value: 5.77 },
-    { name: "x3", value: 2.8 },
-    { name: "x4", value: -15 },
+    { name: "x1", value: 1 },
+    { name: "x2", value: 1.3 },
+    { name: "x3", value: 9.9 },
+    { name: "x4", value: 30 },
   ],
 ];
 
-export const b = [-64.78, 0.654, -24.016, 54.567];
+export const b = [-10.78, 11.554, -20.016, 204.751];
 // export const b = [0.654, -24.016, 54.567, -64.78];
 export const Aarg = A.map((row) => {
   return row.map((item) => {
